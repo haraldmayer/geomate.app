ee495b245d9fe8d00666262ac08fb9f3e8647028
Harald Mayer <harald@hutte.io>
2025-12-14 20:13:26 +0100

    fix build

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,11 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-import node from '@astrojs/node';
 import cloudflare from '@astrojs/cloudflare';
-
-// Cloudflare Pages sets CF_PAGES=1 during build
-const isCloudflare = process.env.CF_PAGES === '1' || process.env.CF_PAGES_BRANCH !== undefined;
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
-  adapter: isCloudflare
-    ? cloudflare({
-        imageService: 'cloudflare'
-      })
-    : node({
-        mode: 'standalone'
-      })
+  adapter: cloudflare({
+    imageService: 'cloudflare'
+  })
 });
