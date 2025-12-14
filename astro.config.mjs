f6f537c8a8d287d0a2c851879a3957941aa7e258
Harald Mayer <harald@hutte.io>
2025-12-14 20:08:06 +0100

    content and style improvements. support build for cloudflare

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,19 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import node from '@astrojs/node';
+import cloudflare from '@astrojs/cloudflare';
 
 const isProduction = process.env.NODE_ENV === 'production';
+const isCloudflare = process.env.CF_PAGES === '1';
 
 // https://astro.build/config
 export default defineConfig({
-  output: isProduction ? 'static' : 'server',
-  adapter: isProduction ? undefined : node({
-    mode: 'standalone'
-  })
+  output: 'server',
+  adapter: isCloudflare
+    ? cloudflare({
+        imageService: 'cloudflare'
+      })
+    : node({
+        mode: 'standalone'
+      })
 });
